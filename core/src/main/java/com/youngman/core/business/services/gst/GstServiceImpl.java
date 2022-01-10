1a30fbab91b7ce1b013c693f9c03f9291a870b03
Ajay Mahato <ajjayymahato@gmail.com>
2022-01-10 10:46:32 +0530

    Services

--- core/src/main/java/com/youngman/core/business/services/gst/GstServiceImpl.java
+++ core/src/main/java/com/youngman/core/business/services/gst/GstServiceImpl.java
@@ -1,7 +1,25 @@
-package com.youngman.core.business.services.gst;
+package com.youngman.core.business.services.shippingaddress;
 
+import com.youngman.core.business.repositories.gst.GstRepository;
+import com.youngman.core.business.repositories.gst.PageableGstRepository;
+import com.youngman.core.business.services.common.generic.YoungmanEntityServiceImpl;
+import com.youngman.core.business.services.gst.GstService;
+import com.youngman.core.model.customerportal.Gst;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+
 @Service
-public class GstServiceImpl {
+public class GstServiceImpl extends YoungmanEntityServiceImpl<Long, Gst> implements GstService{
+
+    private GstRepository gstRepository;
+
+    private PageableGstRepository pageableGstRepository;
+
+    @Inject
+    public GstServiceImpl(GstRepository gstRepository, PageableGstRepository pageableGstRepository) {
+        super(gstRepository);
+        this.gstRepository = gstRepository;
+        this.pageableGstRepository = pageableGstRepository;
+    }
 }
